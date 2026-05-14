3e94aa1edd7084b4c0c3fade3214abf90e0ef2cf
karuppaiya <karuppaiya.fullstackdev@gmail.com>
2026-05-14 18:21:35 +0530

    feat: implement CartPage with cart functionality and loading skeletons; add EmptyCart component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import PageLoader from "./components/PageLoader";
 import Layout from "./components/Layout";
 import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
-// import CartPage from "./pages/CartPage";
+import CartPage from "./pages/CartPage";
 // import OrdersPage from "./pages/OrdersPage";
 // import CheckoutReturnPage from "./pages/CheckoutReturnPage";
 // import ProductDetailPage from "./pages/ProductDetailPage";
@@ -29,6 +29,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </Layout>
   );
