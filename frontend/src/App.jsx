1838d4682548b718d6bd1bbb7b4a20d88d232689
karuppaiya <karuppaiya.fullstackdev@gmail.com>
2026-05-14 19:20:27 +0530

    feat: add OrdersPage component with order fetching logic and display; implement OrderPreview for order items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Layout from "./components/Layout";
 import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
-// import OrdersPage from "./pages/OrdersPage";
+import OrdersPage from "./pages/OrdersPage";
 // import CheckoutReturnPage from "./pages/CheckoutReturnPage";
 // import ProductDetailPage from "./pages/ProductDetailPage";
 // import { SentryDemoPage } from "./pages/SentryDemoPage";
@@ -30,6 +30,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route
+          path="/orders"
+          element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />}
+        />
       </Routes>
     </Layout>
   );
